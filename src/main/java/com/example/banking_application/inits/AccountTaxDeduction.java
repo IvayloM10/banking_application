234eb1fb170b32a0fb8d621e7f749dd3170b63c5
IvayloM10 <ivchoivo@gmail.com>
2024-07-16 19:36:34 +0300

    adding event scheduler

---
+++ src/main/java/com/example/banking_application/inits/AccountTaxDeduction.java
@@ -0,0 +1,28 @@
+package com.example.banking_application.inits;
+
+import com.example.banking_application.models.entities.Account;
+import com.example.banking_application.repositories.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+public class AccountTaxDeduction {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Transactional
+    @Scheduled(cron = "0 0 0 1 * ?")
+    public void reduceAccountBalances() {
+        List<Account> accounts = this.accountRepository.findAll();
+
+        for (Account account : accounts) {
+            account.reduceAccount(1.5);
+            this.accountRepository.save(account);
+        }
+    }
+}
